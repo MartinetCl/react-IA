12c8a4f10046ab61d78c419246cca99eaab58b5c
MartinetC <71493398+MartinetCl@users.noreply.github.com>
2024-02-14 09:30:06 +0100

    add dedicated func for each call

--- chat-api/src/services/chatGptService.ts
+++ chat-api/src/services/chatGptService.ts
@@ -8,18 +8,33 @@ class ChatGptService {
     });
   }
 
-  async sendMessage(question: string): Promise<string> {
+  async sendMessage(text: string, context: string): Promise<string> {
     try {
-      const result = await this.api.sendMessage(question, {
-        systemMessage: 'You will give response and answer to a quizz game.',
+      const result = await this.api.sendMessage(text, {
+        systemMessage: context,
       });
 
       return result.text;
     } catch (error) {
-      // Gestion des erreurs ici
       console.error('Erreur lors de l\'appel à l\'API ChatGPT:', error);
       throw error;
     }
+  }
+
+  async askQuestion(question: string, context: string, difficulty: string): Promise<string> {
+    return this.sendMessage(question, 'Give me a question related to ' + context + ' with a level of difficulty ' + difficulty);
+  }
+
+  async askResponse(question: string): Promise<string> {
+    return this.sendMessage(question, 'Give me the response to the following question.');
+  }
+
+  async askIntel(question: string): Promise<string> {
+    return this.sendMessage(question, 'Give me intel about the following question but not answer it.');
+  }
+
+  async askExplanation(question: string): Promise<string> {
+    return this.sendMessage(question, 'Give me complete response explanation about the following question.');
   }
 }
 
