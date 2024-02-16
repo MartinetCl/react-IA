31f8015c9025a1cd56cdf97e70a9af9c551a1387
IGSparkew <alex.robbrecht@sfr.fr>
2024-02-16 08:32:29 +0100

    add room interaction

--- chat-api/src/room/room.gateway.ts
+++ chat-api/src/room/room.gateway.ts
@@ -15,7 +15,15 @@ interface IPlayer {
     client: Socket, 
     username?: string, 
     score: number,
-    id: number 
+    id: string 
+}
+
+interface IRoom {
+    players: IPlayer[],
+    responses: IResponse[],
+    topics: ITopic[],
+    id: string,
+    password?: string
 }
 
 @WebSocketGateway({cors: true})
@@ -25,6 +33,7 @@ export class RoomGateway {
 
   players:IPlayer[] = [];
   responses: IResponse[] = [];
+  rooms: IRoom[] = [];
 
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
@@ -43,9 +52,52 @@ export class RoomGateway {
     this.server.emit('player-info', player)
   }
 
+  @SubscribeMessage('set-username')
+  handleSetUsername(client: Socket, payload: any) {
+    const player = this.findPlayerById(client.id);
+    player.username = payload.username;
+    this.server.emit('player-info', player);
+  }
+
+  @SubscribeMessage('create-room')
+  handleCreateRoom(client: Socket, payload: any) {
+    const player = this.findPlayerById(client.id);
+    const roomId = uuidv4();
+    const room: IRoom = { 
+        players: [player],
+        responses: [],
+        topics: [],
+        id: roomId
+    }
+    //emit ? 
+  }
+
+  @SubscribeMessage('join-room')
+  handleJoinRoom(client: Socket, payload: any) {
+    const player = this.findPlayerById(client.id);
+    const room = this.findRoomById(payload.roomId);
+    if(room) {
+        if (room.password && room.password !== payload.password) {
+            return;
+        }
+        room.players.push(player);
+        this.server.emit('room-info', room);
+    }
+  }
+
+
   handleDisconnect(client: any) {
     console.log('client disconnected ', client.id);
     this.players = this.players.filter((p) => p.id !== client.id);
+  }
+
+
+  private findPlayerById(id: string): IPlayer {
+    return this.players.find((p) => p.id === id);
+  }
+
+  private findRoomById(id: string): IRoom {
+    return this.rooms.find((r) => r.id === id);
   }
 
 }
