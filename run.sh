b2108c27546b7f186c8edddb18dc1694c3f602ae
IGSparkew <alex.robbrecht@sfr.fr>
2024-02-20 22:31:43 +0100

    add script run

---
+++ run.sh
@@ -0,0 +1,6 @@
+npm install --prefix ./chat-api 
+npm install --prefix ./chat-front 
+
+npm --prefix ./chat-api run start &
+npm --prefix ./chat-front run build &
+npm --prefix ./chat-front run start 
